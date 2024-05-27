60b16f1fae8fc7b0de5708b48e8c12dd027193ad
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-05-28 00:19:18 +0530

    removed beep feature

--- notification_system.py
+++ notification_system.py
@@ -2,8 +2,6 @@ import os
 import cv2
 import requests
 from dotenv import load_dotenv
-import numpy as np
-import pyaudio
 
 class Notification:
     def __init__(self, beep=False, alert=False):
@@ -13,15 +11,6 @@ class Notification:
         self.filename = "Output/fallen_frame.jpg"
         self.webhook_url = os.getenv("API_KEY")
         
-        if self.beep:
-            self.frequency = 2500
-            self.duration = 0.5
-            self.sample_rate = 44100
-            self.t = np.linspace(0, self.duration, int(self.sample_rate * self.duration), endpoint=False)
-            self.samples = (np.sin(2 * np.pi * self.frequency * self.t)).astype(np.float32)
-            self.p = pyaudio.PyAudio()
-            self.stream = self.p.open(format=pyaudio.paFloat32, channels=1, rate=self.sample_rate, output=True)
-    
     def notify(self, modified_frame, flagFall):
         if flagFall and self.beep:
             self.play_beep()
@@ -38,10 +27,4 @@ class Notification:
             os.remove(self.filename)
     
     def play_beep(self):
-        self.stream.write(self.samples.tobytes())
-    
-    def __del__(self):
-        if self.beep:
-            self.stream.stop_stream()
-            self.stream.close()
-            self.p.terminate()
+        pass
